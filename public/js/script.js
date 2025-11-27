00f080dadee0fdb974e5d0acebb7bc2c0c90db1e
Henry Campos <henry.campos_estudan@86WY6L3.pdc-academico.ifms.tl>
2025-11-27 17:53:42 -0400

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,81 @@
-const btn = document.getElementById("btn");
-btn.addEventListener("click", enviarItem);
+const navLinks = document.querySelectorAll('.nav-link');
 
-function enviarItem() {
-  const itemValue = document.getElementById("item").value;
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    navLinks.forEach(l => l.classList.remove('active'));
+    
+    this.classList.add('active');
+  });
+});
 
-  fetch("http://localhost:3000/customer", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ info: itemValue }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Item criado:", data);
-      carregarItems();
-    })
-    .catch((error) => console.error("Erro ao enviar item:", error));
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const slideContainer = document.querySelector('.carousel-slide');
+  const slides = document.querySelectorAll('.slide');
+  const nextBtn = document.getElementById('nextBtn');
+  const prevBtn = document.getElementById('prevBtn');
 
-function carregarItems() {
-  fetch("http://localhost:3000/customer")
-    .then((response) => response.json())
-    .then((items) => {
-      const res = document.getElementById("res");
-      res.innerHTML = "";
-      items.forEach((item) => {
-        const div = document.createElement("div");
-        div.className = "item";
-        div.id = `item-${item.id}`;
-        div.innerHTML = `
-      <h2>item-${item.id}</h2>
-      <p>${item.info}</p>
-      `;
-        res.appendChild(div);
-      });
-    })
-    .catch((error) => console.error("Erro ao carregar itens:", error));
-}
+  let counter = 0;
+  const size = slides[0].clientWidth;
 
-window.onload = carregarItems;
+  nextBtn.addEventListener('click', () => {
+      if (counter >= slides.length - 1) return;
+      counter++;
+      moveSlide();
+  });
+
+  // Listener para o botão esquerdo (anterior)
+  prevBtn.addEventListener('click', () => {
+      if (counter <= 0) return;
+      counter--;
+      moveSlide();
+  });
+
+  function moveSlide() {
+      slideContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
+
+// const btn = document.getElementById("btn");
+// btn.addEventListener("click", enviarItem);
+
+// function enviarItem() {
+//   const itemValue = document.getElementById("item").value;
+
+//   fetch("http://localhost:3000/customer", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ info: itemValue }),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log("Item criado:", data);
+//       carregarItems();
+//     })
+//     .catch((error) => console.error("Erro ao enviar item:", error));
+// }
+
+// function carregarItems() {
+//   fetch("http://localhost:3000/customer")
+//     .then((response) => response.json())
+//     .then((items) => {
+//       const res = document.getElementById("res");
+//       res.innerHTML = "";
+//       items.forEach((item) => {
+//         const div = document.createElement("div");
+//         div.className = "item";
+//         div.id = `item-${item.id}`;
+//         div.innerHTML = `
+//       <h2>item-${item.id}</h2>
+//       <p>${item.info}</p>
+//       `;
+//         res.appendChild(div);
+//       });
+//     })
+//     .catch((error) => console.error("Erro ao carregar itens:", error));
+// }
+
+// window.onload = carregarItems;
