1932b700111a2de47ca81e5d03671330639634c8
Henry <henrytoliveriacampos@gmail.com>
2025-11-28 19:48:39 -0300

    Finalizando o crud dos users

--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -91,7 +91,9 @@ class UsersController {
       return res.status(404).json();
     }
 
-    return res.status(200).json(user);
+    const { id, name, email, createdAt, updatedAt } = user;
+
+    return res.status(200).json({ id, name, email, createdAt, updatedAt });
   }
 
   async create(req, res) {
@@ -104,8 +106,11 @@ class UsersController {
       ),
     });
 
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: "Error on validate schema." });
+    try {
+      await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      console.log(err.errors);
+      return res.status(400).json({ error: err.errors });
     }
 
     const { id, name, email, updatedAt, createdAt } = await User.create(
@@ -117,20 +122,32 @@ class UsersController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
-      oldPassword: Yup.string().min(8),
+      oldPassword: Yup.string().min(8).notRequired(),
+
       password: Yup.string()
         .min(8)
-        .when("oldPassword", (oldPassword, field) =>
-          oldPassword ? field.required() : field
-        ),
+        .notRequired()
+        .when("oldPassword", {
+          is: (value) => !!value, // só exige se oldPassword tiver valor
+          then: (schema) => schema.required(),
+          otherwise: (schema) => schema.notRequired(),
+        }),
 
-      passwordConfirmation: Yup.string().when("password", (password, field) =>
-        password ? field.required().oneOf([Yup.ref("password")]) : field
-      ),
+      passwordConfirmation: Yup.string()
+        .notRequired()
+        .when("password", {
+          is: (value) => !!value,
+          then: (schema) => schema.required().oneOf([Yup.ref("password")]),
+          otherwise: (schema) => schema.notRequired(),
+        }),
     });
 
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: "Error on validate schema." });
+    console.log(req.body);
+    try {
+      await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      console.log(err.errors);
+      return res.status(400).json({ error: err.errors });
     }
 
     const user = await User.findByPk(req.params.id);
